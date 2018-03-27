e597f6c5ba9667f8f0da6cc70c96801f593cbd1c
Mark Zsombor <markszsombor@gmail.com>
2018-03-27 14:34:01 -0700

    app renders list data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <TodoList />
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
-
-
 
 <script>
 import TodoList from './components/TodoList';
@@ -13,6 +11,27 @@ export default {
   name: 'App',
   components: {
     TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
