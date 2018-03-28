4b4e49d2836685ff5c67b23b9ec1efa682f92dec
Mark Zsombor <markszsombor@gmail.com>
2018-03-27 17:24:59 -0700

    todo items are editable

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ui centered card">
-    <div class="content">
+    <!-- Non-edit mode -->
+    <div class="content" v-show="!isEditing">
       <div class="header">
         {{ todo.title }}
       </div>
@@ -8,11 +9,30 @@
         {{ todo.project }}
       </div>
       <div class='extra content'>
-        <span class='right floated edit icon'>
+        <span class='right floated edit icon' v-on:click="showForm">
           <i class='edit icon'></i>
         </span>
       </div>
     </div>
+    <!-- Editing mode -->
+    <div class="content" v-show="isEditing">
+      <div class="ui form">
+        <div class='field'>
+          <label>Title</label>
+          <input type="text" v-model="todo.title">
+        </div>
+        <div class='field'>
+          <label>Projects</label>
+          <input type='text' v-model="todo.project">
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click="hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+    </div>
+
     <div class='ui bottom attached green basic button' v-show="todo.done">
       Completed
     </div>
@@ -26,6 +46,19 @@
 
 export default {
   props: ['todo'],
+  data() {
+    return {
+      isEditing: false,
+    };
+  },
+  methods: {
+    showForm() {
+      this.isEditing = true;
+    },
+    hideForm() {
+      this.isEditing = false;
+    },
+  },
 };
 
 </script>
