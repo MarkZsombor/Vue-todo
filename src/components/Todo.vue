13a97a47e7e838a5006270182da10d400f44227b
Mark Zsombor <markszsombor@gmail.com>
2018-03-28 17:38:39 -0700

    todos can now be marked as completed

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -39,7 +39,8 @@
     <div class='ui bottom attached green basic button' v-show="todo.done">
       Completed
     </div>
-    <div class='ui bottom attached red basic button' v-show="!todo.done">
+    <!-- eslint-disable-next-line -->
+    <div class='ui bottom attached red basic button' v-show="!todo.done" v-on:click="completeTodo(todo)">
       Complete
     </div>
   </div>
@@ -63,6 +64,9 @@ export default {
     },
     deleteTodo(todo) {
       this.$emit('delete-todo', todo);
+    },
+    completeTodo(todo) {
+      this.$emit('complete-todo', todo);
     },
   },
 };
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,7 +3,7 @@
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
     <!-- eslint-disable-next-line -->
-    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id"></todo>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id"></todo>
   </div>
 </template>
 
@@ -20,6 +20,10 @@ export default {
     deleteTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
     },
   },
 };
